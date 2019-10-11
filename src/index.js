8bfbe4a460424c4d7412148eb3010d6a711b888f
Vladimir Kovalchuk <Vladimir6332@gmail.com>
2019-10-11 17:03:31 +0300

    feat: save changes

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let difBracket = {};
+  let sameBracket = {};
+
+  // create configs different and same brackets
+  bracketsConfig.forEach((val, index, arr) => {
+    if (val[0] === val[1]) sameBracket[val[0]] = val[0]
+    else difBracket[val[1]] = val[0];
+  });
+
+  // check simple brackets
+  let countSimpleBrackets = function (arr) {
+    
+  }
+
+
+  
+   
+
 }
